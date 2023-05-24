cfb65b5bcbee77d1757c2254ef0cc4052d8d723a
Yuto Yasuda <yuto.yasuda@rakus-partners.co.jp>
2023-05-24 10:12:05 +0900

    マージのため in feature/dashboard

--- src/pages/dashboard/dbSales.tsx
+++ src/pages/dashboard/dbSales.tsx
@@ -20,74 +20,81 @@ const DbSales = () => {
           <div className="text-xl mr-72 mb-5 ">◾️業務状況</div>
           <form className="flex justify-center mb-5">
             <div className="mr-32">
-              <input type="radio" name="1" />
-              <label>待機中</label>
+              <input type="radio" name="1" id="1" />
+              <label htmlFor="1">待機中</label>
             </div>
             <div>
-              <input type="radio" name="1" />
-              <label>アサイン中</label>
+              <input type="radio" name="1" id="1" />
+              <label htmlFor="1">アサイン中</label>
             </div>
           </form>
 
           <div className="text-xl mr-80 mb-3">◾️職種</div>
           <form className="flex justify-center mb-5 text-xl">
             <div className="mr-4">
-              <input type="radio" name="2" />
-              <label>FR</label>
+              <input type="radio" name="2" id="2" />
+              <label htmlFor="2">FR</label>
             </div>
             <div className="mr-4">
-              <input type="radio" name="2" />
-              <label>JAVA</label>
+              <input type="radio" name="2" id="2" />
+              <label htmlFor="2">JAVA</label>
             </div>
             <div className="mr-4">
-              <input type="radio" name="2" />
-              <label>QA</label>
+              <input type="radio" name="2" id="2" />
+              <label htmlFor="2">QA</label>
             </div>
             <div className="mr-4">
-              <input type="radio" name="2" />
-              <label>ML</label>
+              <input type="radio" name="2" id="2" />
+              <label htmlFor="2">ML</label>
             </div>
             <div className="mr-4">
-              <input type="radio" name="2" />
-              <label>CL</label>
+              <input type="radio" name="2" id="2" />
+              <label htmlFor="2">CL</label>
             </div>
             <div className="mr-4">
-              <input type="radio" name="2" />
-              <label>PHP</label>
+              <input type="radio" name="2" id="2" />
+              <label htmlFor="2">PHP</label>
             </div>
           </form>
 
           <div className="text-xl mr-80 mb-3">◾️スキル</div>
           <form className="flex justify-center mb-5 text-xl">
-            <div className="mr-4">
-              <input type="checkbox" />
-              <label>React</label>
-            </div>
-            <div className="mr-4">
-              <input type="checkbox" />
-              <label>React</label>
-            </div>
-            <div className="mr-4">
-              <input type="checkbox" />
-              <label>React</label>
-            </div>
-            <div className="mr-4">
-              <input type="checkbox" />
-              <label>React</label>
-            </div>
-            <div className="mr-4">
-              <input type="checkbox" />
-              <label>React</label>
-            </div>
-            <div className="mr-4">
-              <input type="checkbox" />
-              <label>React</label>
-            </div>
-            <div className="mr-4">
-              <input type="checkbox" />
-              <label>React</label>
+            <div className="grid grid-cols-4">
+              <div className="mr-4">
+                <input type="checkbox" id="3" />
+                <label htmlFor="3">React</label>
+              </div>
+              <div className="mr-4">
+                <input type="checkbox" id="4" />
+                <label htmlFor="4">React</label>
+              </div>
+              <div className="mr-4">
+                <input type="checkbox" id="5" />
+                <label htmlFor="5">React</label>
+              </div>
+              <div className="mr-4">
+                <input type="checkbox" id="6" />
+                <label htmlFor="7">React</label>
+              </div>
+              <div className="mr-4">
+                <input type="checkbox" id="7" />
+                <label htmlFor="7">React</label>
+              </div>
+              <div className="mr-4">
+                <input type="checkbox" id="8" />
+                <label htmlFor="8">React</label>
+              </div>
+              <div className="mr-4">
+                <input type="checkbox" id="9" />
+                <label htmlFor="9">React</label>
+              </div>
             </div>
           </form>
+          <div>
+            <input type="checkbox" />
+            その他
+            <input type="text" className="ml-3"/>
+          </div>
 
           <button className="mt-5 px-3 py-1 shadow-md cursor-pointer bg-gradient-to-b from-orange-400 to-yellow-400 rounded-xl border-2 border-white border-solid text-white">
             検索する
