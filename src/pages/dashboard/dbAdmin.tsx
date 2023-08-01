17187be71f982abef7fc024b403841190c4139aa
Yuto Yasuda <yuto.yasuda@rakus-partners.co.jp>
2023-08-01 16:55:41 +0900

    エンジニアDB・申請通知テスト in test2

--- src/pages/dashboard/dbAdmin.tsx
+++ src/pages/dashboard/dbAdmin.tsx
@@ -15,7 +15,28 @@ const DbAdmin = () => {
   const { data, error } = useSWR('/api/request', fetcher);
 
   if (!data) {
-    return <div>Loading...</div>; // データの取得中に表示
+    return (
+      <>
+        <Header />
+        <section>
+          <div className="flex lg:justify-between justify-center font-bold flex-wrap max-w-4xl m-auto mt-10 lg:mt-20">
+            <div className="relative">
+              <button className="w-80 py-4 mt-10 lg:mt-20 mx-10 text-orange-50 text-4xl text-center rounded-2xl shadow-lg cursor-pointer bg-gradient-to-b from-orange-400 to-yellow-400 border-4 border-orange-50  hover:scale-110 transition-all">
+                <Link href="/notificationList">申請通知</Link>
+              </button>
+              <Link href="/notificationEngineer"></Link>
+            </div>
+
+            <button className="w-80 py-4 mt-10 lg:mt-20 mx-10  text-orange-50 text-4xl text-center rounded-2xl shadow-lg cursor-pointer bg-gradient-to-b from-orange-400 to-yellow-400 border-4 border-orange-50  hover:scale-110 transition-all">
+              <Link href="/dashboard/dbSales">エンジニア検索</Link>
+            </button>
+          </div>
+        </section>
+        <div className="absolute w-full" style={{ bottom: '0%' }}>
+          <Footer />
+        </div>
+      </>
+    ); // データの取得中に表示
   }
 
   // ステータスが申請中のみに絞る
@@ -32,10 +53,10 @@ const DbAdmin = () => {
             <button className="w-80 py-4 mt-10 lg:mt-20 mx-10 text-orange-50 text-4xl text-center rounded-2xl shadow-lg cursor-pointer bg-gradient-to-b from-orange-400 to-yellow-400 border-4 border-orange-50  hover:scale-110 transition-all">
               <Link href="/notificationList">申請通知</Link>
             </button>
-            <div
-              className="absolute rounded-full bg-blue-500 text-blue-50 py-1 px-3 text-2xl text-center lg:right-6 lg:top-16 right-6 top-8 cursor-pointer"
-            >
-                <Link href="/notificationEngineer">{requests.length}</Link>
+            <div className="absolute rounded-full bg-blue-500 text-blue-50 py-1 px-3 text-2xl text-center lg:right-6 lg:top-16 right-6 top-8 cursor-pointer">
+              <Link href="/notificationEngineer">
+                {requests.length}
+              </Link>
             </div>
           </div>
 
