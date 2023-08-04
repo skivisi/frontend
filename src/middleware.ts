b2e2aa777f4fad214242adda3c6f721b8c77a3ce
mhatanorks <masaki.hatano@rakus-partners.co.jp>
2023-08-04 16:28:24 +0900

    トップアクセス時のリダイレクトミドルウェア設置

---
+++ src/middleware.ts
@@ -0,0 +1,25 @@
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+
+// トップページにアクセスされた際の挙動
+
+export function middleware(request: NextRequest) {
+  // 管理者クッキーを持っている場合、管理者ダッシュボードにリダイレクト
+  if (request.cookies.has('adminId')) {
+    return NextResponse.rewrite(
+      new URL('/dashboard/dbAdmin', request.url)
+    );
+    // ユーザークッキーを持っている場合、ユーザーダッシュボードにリダイレクト
+  } else if (request.cookies.has('userId')) {
+    return NextResponse.rewrite(
+      new URL('/dashboard/dbEngineer', request.url)
+    );
+    // 何もない場合ログインページにリダイレクト
+  } else {
+    return NextResponse.rewrite(new URL('/loginAdmin', request.url));
+  }
+}
+
+export const config = {
+  matcher: '/',
+};
